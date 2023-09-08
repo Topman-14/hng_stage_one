25300c3e55602bda9a5d9e525830340c4a2099e2
tope101 <temitope@kobo360.com>
2023-09-08 23:12:58 +0100

    finishing touches

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
 app.get('/api', (req, res) => {
  
     try{
+    const now = new Date();
     const slack_name = req.query.slack_name;
     const track = req.query.track;
-    const now = new Date();
-    const current_day = daysOfWeek[now.getUTCDay()];
+    const current_day = now.toLocaleDateString('en-US', { weekday: 'long', timeZone: 'UTC' });
+    const utc_time = now.toISOString().slice(0, 19) + "Z";
+    const github_file_url = "https://github.com/Topman-14/hng_stage_one/blob/main/index.js";
+    const github_repo_url = "https://github.com/Topman-14/hng_stage_one";
+    const status_code = 200;
 
         res.status(200).json({
             slack_name,
             current_day,
-            track
+            utc_time,
+            track,
+            github_file_url,
+            github_repo_url,
+            status_code
         });
 
     } catch (error) {
@@ -29,13 +34,3 @@ app.get('/api', (req, res) => {
 app.listen(PORT, () => {
     console.log("Server listening on port: " + PORT);
 });
-
-// {
-//     "slack_name": "example_name",
-//     "current_day": "Monday",
-//     "utc_time": "2023-08-21T15:04:05Z",
-//     "track": "backend",
-//     "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-//     "github_repo_url": "https://github.com/username/repo",
-//     “status_code”: 200
-//   }
