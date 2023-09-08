64c056ce5e5c4f726eebdef2262f88f7e4884920
tope101 <temitope@kobo360.com>
2023-09-08 23:17:15 +0100

    finishing touches

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 app.get('/api', (req, res) => {
  
