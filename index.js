a52fea1af519b10b85092a27b81720f59357164c
tope101 <temitope@kobo360.com>
2023-09-09 01:54:04 +0100

    work abegg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 app.get('/api', (req, res) => {
  
@@ -8,7 +9,8 @@ app.get('/api', (req, res) => {
     const now = new Date();
     const slack_name = req.query.slack_name;
     const track = req.query.track;
-    const current_day = now.toLocaleDateString('en-US', { weekday: 'long', timeZone: 'UTC' });
+    // const current_day = now.toLocaleDateString('en-US', { weekday: 'long', timeZone: 'UTC' });
+    const current_day = daysOfWeek[new Date(now).getUTCDay()];
     const utc_time = now.toISOString();
     const github_file_url = "https://github.com/Topman-14/hng_stage_one/blob/main/index.js";
     const github_repo_url = "https://github.com/Topman-14/hng_stage_one";
@@ -34,5 +36,22 @@ app.listen(PORT, () => {
     console.log("Server listening on port: " + PORT);
 });
 
-// 2023-09-08T22:49:31.320Z
-// 2023-08-21T15:04:05Z
+// {
+//     "slack_name": "example_name",
+//     "current_day": "Monday",
+//     "utc_time": "2023-08-21T15:04:05Z",
+//     "track": "backend",
+//     "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+//     "github_repo_url": "https://github.com/username/repo",
+//     “status_code”: 200
+//   }
+
+// {
+//     "slack_name": "Topman",
+//     "current_day": "Saturday",
+//     "utc_time": "2023-09-09T00:43:20.592Z",
+//     "track": "backend",
+//     "github_file_url": "https://github.com/Topman-14/hng_stage_one/blob/main/index.js",
+//     "github_repo_url": "https://github.com/Topman-14/hng_stage_one",
+//     "status_code": 200
+// }
